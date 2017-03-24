0265058b6790a5452a31b60832aeeb54e199769b
Alberto Leiva Popper <ydahhrk@gmail.com>
2017-03-24 10:17:40 -0600

    Revert updateSchemaSetting() and updateSchemaSetting()
    I shouldn't have deleted them.

--- src/main/java/mx/nic/rdap/db/model/DBConfigurationModel.java
+++ src/main/java/mx/nic/rdap/db/model/DBConfigurationModel.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,6 +19,7 @@ public class DBConfigurationModel {
 	private final static String QUERY_GROUP = "DBConfiguration";
 
 	private static final String GET_PROPERTY_QUERY = "getProperty";
+	private static final String UPDATE_PROPERTY_QUERY = "updateProperty";
 
 	private static final String SCHEMA_SETTING_KEY = "schema";
 
@@ -43,6 +46,11 @@ public class DBConfigurationModel {
 		return getPropertyFromDb(SCHEMA_SETTING_KEY, connection);
 	}
 
+	public static void updateSchemaSetting(String newSchemaValue, Connection connection)
+			throws ObjectNotFoundException, SQLException {
+		updatePropertyFromDb(newSchemaValue, SCHEMA_SETTING_KEY, connection);
+	}
+
 	private static String getPropertyFromDb(String propertyName, Connection connection)
 			throws SQLException, ObjectNotFoundException {
 		String query = getQueryGroup().getQuery(GET_PROPERTY_QUERY);
@@ -60,4 +68,29 @@ public class DBConfigurationModel {
 		return setting;
 	}
 
+	private static void updatePropertyFromDb(String newValue, String propertyName, Connection connection)
+			throws ObjectNotFoundException, SQLException {
+		String oldValue;
+		try {
+			oldValue = getPropertyFromDb(propertyName, connection);
+		} catch (ObjectNotFoundException e) {
+			oldValue = null;
+		}
+
+		String query = getQueryGroup().getQuery(UPDATE_PROPERTY_QUERY);
+		try (PreparedStatement statement = connection.prepareStatement(query);) {
+			statement.setString(1, newValue);
+			if (oldValue != null) {
+				statement.setString(2, oldValue);
+			} else {
+				statement.setNull(2, Types.VARCHAR);
+			}
+			statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+			statement.setString(4, propertyName);
+
+			logger.log(Level.INFO, "Executing query: " + statement.toString());
+			int executeUpdate = statement.executeUpdate();
+			logger.log(Level.INFO, "Rows updated: " + executeUpdate);
+		}
+	}
 }
--- src/main/resources/META-INF/sql/DBConfiguration.sql
+++ src/main/resources/META-INF/sql/DBConfiguration.sql
@@ -3,3 +3,6 @@ SELECT * FROM {schema}.configuration conf WHERE conf.con_name = ?;
 
 #getAll
 SELECT * FROM {schema}.configuration;
+
+#updateProperty
+UPDATE {schema}.configuration SET con_value = ? , con_previous_value = ?, con_last_update = ? WHERE con_name = ?;
