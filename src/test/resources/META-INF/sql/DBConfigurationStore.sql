6b6ea4ea2ba74089042b991580beb36a75bfb352
Paco Moreno <fjarana@itesm.mx>
2017-10-20 11:41:51 -0500

    Remove references to 'configuration' table

--- src/test/resources/META-INF/sql/DBConfigurationStore.sql
+++
@@ -1,8 +0,0 @@
-#getProperty
-SELECT con_name, con_value, con_previous_value, con_description, con_create_time, con_last_update FROM {schema}.configuration conf WHERE conf.con_name = ?;
-
-#getAll
-SELECT con_name, con_value, con_previous_value, con_description, con_create_time, con_last_update FROM {schema}.configuration;
-
-#updateProperty
-UPDATE {schema}.configuration SET con_value = ? , con_previous_value = ?, con_last_update = ? WHERE con_name = ?;
